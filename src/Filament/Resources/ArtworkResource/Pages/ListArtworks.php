07e709eff39570f295b974e2059f0033ba5d19c7
Frédéric Blanc <frederic.blanc@itanea.fr>
2026-06-27 17:46:59 +0200

    fix: add CreateAction header to ListArtworks page

--- src/Filament/Resources/ArtworkResource/Pages/ListArtworks.php
+++ src/Filament/Resources/ArtworkResource/Pages/ListArtworks.php
@@ -2,10 +2,18 @@
 
 namespace Happytodev\BlogrArtist\Filament\Resources\ArtworkResource\Pages;
 
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Happytodev\BlogrArtist\Filament\Resources\ArtworkResource;
 
 class ListArtworks extends ListRecords
 {
     protected static string $resource = ArtworkResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
