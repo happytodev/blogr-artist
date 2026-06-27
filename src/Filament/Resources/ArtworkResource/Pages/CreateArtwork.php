8c0138291f483d14eb1cb56f64bb8b8c7345ffd4
Frédéric Blanc <frederic.blanc@itanea.fr>
2026-06-27 22:41:11 +0200

    fix: move category_id inside translations Repeater with custom options() and mutate hooks

--- src/Filament/Resources/ArtworkResource/Pages/CreateArtwork.php
+++ src/Filament/Resources/ArtworkResource/Pages/CreateArtwork.php
@@ -8,4 +8,17 @@ use Happytodev\BlogrArtist\Filament\Resources\ArtworkResource;
 class CreateArtwork extends CreateRecord
 {
     protected static string $resource = ArtworkResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (isset($data['translations']) && is_array($data['translations'])) {
+            $first = reset($data['translations']);
+            if (isset($first['category_id'])) {
+                $data['category_id'] = $first['category_id'];
+            }
+            array_walk($data['translations'], fn (&$t) => unset($t['category_id']));
+        }
+
+        return $data;
+    }
 }
--- src/Filament/Resources/ArtworkResource/Pages/EditArtwork.php
+++ src/Filament/Resources/ArtworkResource/Pages/EditArtwork.php
@@ -8,4 +8,17 @@ use Happytodev\BlogrArtist\Filament\Resources\ArtworkResource;
 class EditArtwork extends EditRecord
 {
     protected static string $resource = ArtworkResource::class;
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (isset($data['translations']) && is_array($data['translations'])) {
+            $first = reset($data['translations']);
+            if (isset($first['category_id'])) {
+                $data['category_id'] = $first['category_id'];
+            }
+            array_walk($data['translations'], fn (&$t) => unset($t['category_id']));
+        }
+
+        return $data;
+    }
 }
